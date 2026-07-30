4f07d5f366203781e8c0f581a46631f66f78012d
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-07-30 14:24:27 +0530

    fix: configure CORS to allow requests from specified client origin

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  credentials: true,
+}));
 
 // Test route
 app.get("/", (req, res) => {
